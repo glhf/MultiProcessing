e504f274b9eadbeb82b788a34277d0f972fe4abf
glhf <goodvin4@gmail.com>
2015-01-14 11:11:32 +0200

    Image processing do by threads! but main thread write output image before process-treads finish image. bug

--- src/main/java/com/glhf/multiprocessing/ImgEngine.java
+++ src/main/java/com/glhf/multiprocessing/ImgEngine.java
@@ -9,10 +9,12 @@ import java.awt.image.BufferedImage;
 import java.awt.image.Raster;
 import java.io.*;
 import java.net.URL;
+import java.util.List;
+import java.util.LinkedList;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 /**
- * Hello world!
+ * 
  */
 public class ImgEngine {
     private static final Logger LOG = LogManager.getLogger(ImgEngine.class);
@@ -31,12 +33,16 @@ public class ImgEngine {
 
         Raster data = img.getData();
         BufferedImage newImg = new BufferedImage(data.getWidth(), data.getHeight(), BufferedImage.TYPE_INT_RGB);
+        new Clerk(img, newImg).start();
 
-        ConcurrentLinkedQueue<Data> queue = new ConcurrentLinkedQueue<>();
-        for (int i = 0; i < data.getWidth(); i++) {
-            for (int j = 0; j < data.getHeight(); j++){
-                queue.add(new Data(i,j,new Color(img.getRGB(i,j))));
+        /*for (int i = 0; i < data.getWidth(); i++) {
+            for (int j = 0; j < data.getHeight(); j++) {
+                newImg.setRGB(i,j, new Data(i, j, new Color(img.getRGB(i, j))).getGray().getRGB());
             }
+        }*/
+        long i=0;
+        while (i < 1000000000){
+            i++;
         }
 
         try {
@@ -47,37 +53,107 @@ public class ImgEngine {
         }
     }
 
-    Color getGray(Color col){
-            int red = (int) (col.getRed() * 0.299);
-            int green = (int) (col.getGreen() * 0.587);
-            int blue = (int) (col.getBlue() * 0.114);
-            return new Color(red + green + blue, red + green + blue, red + green + blue);
+    Color getGray(Color col) {
+        int red = (int) (col.getRed() * 0.299);
+        int green = (int) (col.getGreen() * 0.587);
+        int blue = (int) (col.getBlue() * 0.114);
+        return new Color(red + green + blue, red + green + blue, red + green + blue);
     }
 
-    class Data{
-        Point point = new Point(0,0);
+    class Data {
+        Point point = new Point(0, 0);
         Color color;
 
-        Data(int x, int y, Color color){
-            this.point.setLocation(x,y);
+        Data(int x, int y, Color color) {
+            this.point.setLocation(x, y);
             this.color = color;
         }
 
-        int getX(){
-            return (int)this.point.getX();
+        Data(Color color) {
+            this.color = color;
         }
 
-        int getY(){
-            return (int)this.point.getY();
+        int getX() {
+            return (int) this.point.getX();
+        }
+
+        int getY() {
+            return (int) this.point.getY();
+        }
+
+        Color getGray() {
+            int red = (int) (this.color.getRed() * 0.299);
+            int green = (int) (this.color.getGreen() * 0.587);
+            int blue = (int) (this.color.getBlue() * 0.114);
+            return new Color(red + green + blue, red + green + blue, red + green + blue);
         }
     }
 
     /**
      * extend thread for preparing queue
      */
-    class Clerk{}
+    class Clerk {
+        final BufferedImage img;
+        final BufferedImage out;
 
-    /**
-     * extend tread for count gray image
-     */
+        Clerk(BufferedImage img, BufferedImage out) {
+            this.img = img;
+            this.out = out;
+        }
+
+        /**
+         * divide indexes of image for calculating with n process
+         */
+        private void start() {
+            List<Point> list = new LinkedList<>();
+            list = getBounds(img.getHeight(), Runtime.getRuntime().availableProcessors());
+            list.forEach(el -> {
+                LOG.info(el.toString());
+                new Thread() {
+                    public void run() {
+                        LOG.info("start thread "+el.x+" "+el.y+" "+img.getWidth());
+                        for (int i = 0; i < img.getWidth(); i++) {
+                            for (int j = el.x; j < el.y; j++) {
+                                out.setRGB(i,j, new Data(i, j, new Color(img.getRGB(i, j))).getGray().getRGB());
+                            }
+                        }
+                    }
+                }.start();
+            });
+        }
+
+        private List<Point> getBounds(int maxIndexValue, int count) {
+            List<Point> list = new LinkedList<>();
+            double n = count;
+            double x = maxIndexValue;
+            double diff = x / n;
+            int start = 0;
+            int end = 0;
+            if ((diff % 1) == 0) {
+                for (int i = 0; i < n; i++) { // startIndex = diff*i  endIndex diff*(i+1)
+                    if (i == 0) {//start index = 0;
+                        list.add(new Point((int) (diff * i), (int) (diff * (i + 1))));
+                    } else {
+                        list.add(new Point((int) ((diff * i) + 1), (int) (diff * (i + 1))));
+                    }
+                }
+            } else {
+                int d = (int) diff;
+                start = 0;
+                end = d;
+                while (end < x) {
+                    //put hear thread init
+                    list.add(new Point(start,end));
+                    if (start == 0) {
+                        start++;
+                    }
+                    start += d;
+                    end += d;
+                }
+                end = (int) x;
+                list.add(new Point(start, end));
+            }
+            return list;
+        }
+    }
 }
